c1e37b1c673639413d35b70354ce17dac6dbcd33
RAJUZZOSOM <rajuzzosom@gmail.com>
2025-09-02 15:21:23 +0530

    Variable Keyword in JavaScript

---
+++ JavaScript/Class 1/Script.js
@@ -0,0 +1,36 @@
+console.log("Hello World...");
+
+//! Variable Keyword
+// ? var
+var a = 10
+// console.log(a);
+
+// a = Variable
+// 10 = value 
+
+var a
+a = 30
+// console.log(a);
+
+
+//? let
+let b = "Hi"
+console.log(b);
+// let b  // not allowed
+
+b = 10
+console.log(b);
+
+// let b = re declare  We can't re declare in let variale keywod
+// "Hi" = re initialize  allow to re initialize
+
+// ? const
+const c = "Hello"
+// console.log(c);
+// const c re initialize //not allowed
+// c = 20 re initialize  //not allowed
+
+// const c = 44;
+// c = 34;
+console.log(c);
+
